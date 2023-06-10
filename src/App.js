caa1a684d54a97f59a5189744d8a21bad21d67f5
Naziullah Shawan <naziullahshawn135@gmail.com>
2023-06-10 11:24:36 +0600

    navbar and footer implement

--- src/App.js
+++ src/App.js
@@ -1,5 +1,3 @@
-import logo from "./logo.svg";
-import "./App.css";
 import { RouterProvider } from "react-router-dom";
 import { routes } from "./routes/routes";
 
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,29 +1,25 @@
-import React from 'react';
+import React from "react";
+import logo from "../assets/short-logo.png";
+import { Link } from "react-router-dom";
 
 const NavBar = () => {
-    return (
-        <div className="navbar bg-base-100">
-        <div className="flex-1">
-          <a className="btn btn-ghost normal-case text-xl">daisyUI</a>
-        </div>
-        <div className="flex-none">
-          <ul className="menu menu-horizontal px-1">
-            <li><a>Link</a></li>
-            <li>
-              <details>
-                <summary>
-                  Parent
-                </summary>
-                <ul className="p-2 bg-base-100">
-                  <li><a>Link 1</a></li>
-                  <li><a>Link 2</a></li>
-                </ul>
-              </details>
-            </li>
-          </ul>
-        </div>
+  return (
+    <div className="navbar">
+      <div className="flex-1 rounded-full">
+        <Link to="/"><img className="w-48" src={logo} alt="logo" /></Link>
       </div>
-    );
+      <div className="flex-none">
+        <ul className="menu text-base font-bold menu-horizontal px-1">
+          <li >
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About Us</Link>
+            </li>
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export default NavBar;
